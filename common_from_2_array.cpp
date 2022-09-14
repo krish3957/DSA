7bbee54cd262a75144268284c36f96ca1509797a
krish3957 <kpparekh81602@gmail.com>
2022-09-14 17:28:29 +0530

    Give common element from the two arrays

---
+++ common_from_2_array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int a[n],b[m];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int j=0;j<m;j++)
+    {
+        cin>>b[j];
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(a[i]==b[j])
+            {
+                cout<<a[i]<<endl;
+            }
+        }
+    }
+    return 0;
+}
